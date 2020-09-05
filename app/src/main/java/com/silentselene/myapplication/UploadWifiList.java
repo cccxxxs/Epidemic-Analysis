467efe1eb93c5e730a085de5da43b363f1d0a562
AkibaSummer <oopsvpwowlq@gmail.com>
2020-09-05 15:56:44 +0800

    restructure uploadLog

--- app/src/main/java/com/silentselene/myapplication/UploadWifiList.java
+++ app/src/main/java/com/silentselene/myapplication/UploadWifiList.java
@@ -11,9 +11,15 @@ import com.silentselene.myapplication.WifiPlugin.DBAdapter;
 import com.silentselene.myapplication.WifiPlugin.WiFiRecord;
 import com.silentselene.myapplication.data.localHistory;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -24,6 +30,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 class UploadWifiList {
+
     static void uploadWifiList(final MainActivity context, int userId, int state) {
         DBAdapter dbAdapter = new DBAdapter(context);
         dbAdapter.open();
@@ -32,64 +39,67 @@ class UploadWifiList {
         if (wiFiRecords == null) {
             return;
         }
-        for (WiFiRecord wiFiRecord : wiFiRecords) {
-            uploadOneWifi(context, wiFiRecord, userId, state);
-            if (wiFiRecord.getTime().compareTo(date) > 0) {
-                date = wiFiRecord.getTime();
-//                try {
-//                    Thread.sleep(200);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
+        JSONArray jsonArray = new JSONArray();
+
+        try {
+            for (WiFiRecord wiFiRecord : wiFiRecords) {
+                String stateString = "";
+                switch (state) {
+                    case AccessRecord.SAFE:
+                        stateString = "healthy";
+                        break;
+                    case AccessRecord.CONFIRMED:
+                        stateString = "ill";
+                        break;
+                    case AccessRecord.HIGH_RISK:
+                        stateString = "high_risk";
+                        break;
+                    case AccessRecord.CURED:
+                        stateString = "cured";
+                        break;
+                }
+                JSONObject item = new JSONObject();
+                item.put("mac", wiFiRecord.getBSSID());
+                item.put("visitTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(wiFiRecord.getTime()));
+                item.put("state", stateString);
+                jsonArray.put(item);
+                if (wiFiRecord.getTime().compareTo(date) > 0) {
+                    date = wiFiRecord.getTime();
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        localHistory.setLastUpdateDate(context, date.getTime());
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(context, "wifi数据上传成功", Toast.LENGTH_SHORT).show();
-            }
-        });
+        doUpload(context, String.valueOf(userId), jsonArray.toString(), date);
     }
 
-
-    static void uploadOneWifi(final MainActivity context, final WiFiRecord wiFiRecord, final int userId, final int state) {
+    static void doUpload(final Context context, final String userId, final String extra, final Date date) {
         OkHttpClient okHttpClient = new OkHttpClient();
-        String stateString = "";
-        switch (state) {
-            case AccessRecord.SAFE:
-                stateString = "healthy";
-                break;
-            case AccessRecord.CONFIRMED:
-                stateString = "ill";
-                break;
-            case AccessRecord.HIGH_RISK:
-                stateString = "high_risk";
-                break;
-            case AccessRecord.CURED:
-                stateString = "cured";
-                break;
-        }
-        final String upstate = stateString;
         RequestBody requestBody = new FormBody.Builder()
-                .add("fkid1", String.valueOf(userId))
-                .add("mac", wiFiRecord.getBSSID())
-                .add("visitTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(wiFiRecord.getTime()))
-                .add("state", stateString)
+                .add("userId", String.valueOf(userId))
+                .add("extra", extra)
                 .build();
-        Request request = new Request.Builder().url("http://123.56.117.101:8080/updateLog").post(requestBody).build();
+        Request request = new Request.Builder().url("http://123.56.117.101:8081/uploadLogs").post(requestBody).build();
         Call call = okHttpClient.newCall(request);//发送请求
         call.enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.d("POST_updateLog_faild", "result: " + wiFiRecord.toString() + upstate);
-                uploadOneWifi(context, wiFiRecord, userId, state);
+                Log.d("POST_updateLog_faild", "result: " + extra);
+                doUpload(context, userId, extra, date);
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                Log.d("POST_updateLog_success", "result: " + wiFiRecord.toString() + upstate+response.body().string());
+                localHistory.setLastUpdateDate(context, date.getTime());
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(context, "wifi数据上传成功", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.d("POST_updateLog_success", "result: " + extra + response.body().string());
             }
         });
     }
 }
+
